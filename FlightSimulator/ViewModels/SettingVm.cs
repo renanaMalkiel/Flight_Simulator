3a32eefe46b1904320b3db91780b475679fe2305
renana malkiel <renana.malkiel@live.biu.ac.il>
2019-04-09 16:19:40 +0300

    settings button and setting popup window

---
+++ FlightSimulator/ViewModels/SettingVm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FlightSimulator.Model;
+using FlightSimulator.Views;
+
+namespace FlightSimulator.ViewModels
+{
+    class SettingVM
+    {
+        private ICommand _settingComand;
+        public ICommand SettingCommand
+        {
+            get
+            {
+                return _settingComand ?? (_settingComand =
+                new CommandHandler(() => OnClick()));
+            }
+            set
+            {
+            }
+        }
+        private void OnClick()
+        {
+            SettingPopup setting = new SettingPopup();
+            setting.ShowDialog();
+        }
+    }
+}
