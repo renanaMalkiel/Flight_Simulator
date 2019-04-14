4f5b187ed41ed4622ac2dc73394e4d1655405992
jennifer klein <jenniejoyness@gmail.com>
2019-04-14 07:40:46 +0300

    commandchannel

--- FlightSimulator/ViewModels/ConnectVM.cs
+++ FlightSimulator/ViewModels/ConnectVM.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FlightSimulator.Model
+namespace FlightSimulator.ViewModels
 {
-    class tets
+    class ConnectVM
     {
-        
-        
     }
 }
